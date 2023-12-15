6aaae5f7b86607b13561bdd5ba3b99e1d88d9d24
quanganhnguyen001 <quanganhnguyen2382001@gmail.com>
2023-12-15 10:36:55 +0700

    [NEW] Add direction function

--- lib/features/location/view/trash_location.dart
+++ lib/features/location/view/trash_location.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:trash_collector_app/features/location/cubit/trash_location_cubit.dart';
-
+import 'package:http/http.dart' as http;
+import '../../../api_key.dart';
 import '../../../gen/assets/assets.gen.dart';
 
 class TrashLocationScreen extends StatefulWidget {
@@ -17,13 +20,17 @@ class TrashLocationScreen extends StatefulWidget {
 }
 
 class _TrashLocationScreenState extends State<TrashLocationScreen> {
+  List listOfPoints = [];
+  String? distance;
+  String? duration;
+  List<LatLng> points = const <LatLng>[];
   BitmapDescriptor makerIcon = BitmapDescriptor.defaultMarker;
   late GoogleMapController _googleMapController;
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(21.0073516, 105.8221555),
-    zoom: 16,
+    zoom: 15,
   );
 
   @override
@@ -63,21 +70,56 @@ class _TrashLocationScreenState extends State<TrashLocationScreen> {
           return Stack(
             children: [
               GoogleMap(
+                polylines: {
+                  Polyline(
+                      consumeTapEvents: true,
+                      color: Colors.red,
+                      polylineId: const PolylineId('_kPolyline'),
+                      points: points,
+                      width: 3)
+                },
                 myLocationEnabled: true,
                 myLocationButtonEnabled: false,
                 markers: {
                   Marker(
                       markerId: MarkerId('1'),
                       icon: makerIcon,
+                      infoWindow: InfoWindow(
+                          title: 'Trường đại học thủy lợi',
+                          onTap: () async {
+                            Position position = await context
+                                .read<TrashLocationCubit>()
+                                .determinePosition();
+                            getDirections(position.latitude, position.longitude,
+                                21.0073516, 105.8221555);
+                          }),
                       position: LatLng(21.0073516, 105.8221555)),
                   Marker(
                       markerId: MarkerId('2'),
                       icon: makerIcon,
-                      position: LatLng(21.0062696, 105.824553)),
+                      infoWindow: InfoWindow(
+                          title: 'Học viện ngân hàng',
+                          onTap: () async {
+                            Position position = await context
+                                .read<TrashLocationCubit>()
+                                .determinePosition();
+                            getDirections(position.latitude, position.longitude,
+                                21.0091314, 105.8262383);
+                          }),
+                      position: LatLng(21.0091314, 105.8262383)),
                   Marker(
                       markerId: MarkerId('3'),
                       icon: makerIcon,
-                      position: LatLng(21.0092606, 105.8180224)),
+                      infoWindow: InfoWindow(
+                          title: 'Trường đại học công đoàn',
+                          onTap: () async {
+                            Position position = await context
+                                .read<TrashLocationCubit>()
+                                .determinePosition();
+                            getDirections(position.latitude, position.longitude,
+                                21.0097431, 105.8231375);
+                          }),
+                      position: LatLng(21.0097431, 105.8231375)),
                 },
                 mapType: MapType.normal,
                 initialCameraPosition: _kGooglePlex,
@@ -113,10 +155,47 @@ class _TrashLocationScreenState extends State<TrashLocationScreen> {
                           color: Colors.blue,
                         )),
                   )),
+              duration != null && distance != null
+                  ? Positioned(
+                      left: 120,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 6, horizontal: 12),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                            'Distance: $distance km\n Duration: $duration min'),
+                      ))
+                  : Container(),
             ],
           );
         },
       ),
     );
+  }
+
+  getDirections(
+      double startLat, double startLng, double endLat, double endLng) async {
+    EasyLoading.show();
+    String url =
+        'https://api.openrouteservice.org/v2/directions/driving-car?api_key=$openroute_api_key&start=$startLng,$startLat&end=$endLng,$endLat';
+    var res = await http.get(Uri.parse(url));
+
+    setState(() {
+      var json = jsonDecode(res.body);
+      listOfPoints = json['features'][0]['geometry']['coordinates'];
+      points = listOfPoints
+          .map((p) => LatLng(p[1].toDouble(), p[0].toDouble()))
+          .toList();
+      distance =
+          (json['features'][0]['properties']['summary']['distance'] / 1000)
+              .toStringAsFixed(2);
+
+      duration = (json['features'][0]['properties']['summary']['duration'] / 60)
+          .toStringAsFixed(0);
+    });
+    EasyLoading.dismiss();
   }
 }
